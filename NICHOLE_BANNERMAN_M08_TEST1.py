af7ba45d5b23f5701495d4c9f5eb2c5bce744bea
afrakums <bannermannichole514@gmail.com>
2025-05-01 21:43:20 +0000

    Add files via upload

---
+++ NICHOLE_BANNERMAN_M08_TEST1.py
@@ -0,0 +1,11 @@
+def greaterThan(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+# Test values
+a = 2
+b = 3
+result = greaterThan(a, b)
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
